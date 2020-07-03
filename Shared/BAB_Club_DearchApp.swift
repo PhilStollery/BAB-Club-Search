43f6134acb6dcf277239cf80f99e83cfd3abc918
Phil Stollery <phil@stollerys.co.uk>
2020-07-03 16:23:23 +0100

    Initial Commit

---
+++ Shared/BAB_Club_DearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BAB_Club_DearchApp.swift
+//  Shared
+//
+//  Created by Phil Stollery on 03/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct BAB_Club_DearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Phil Stollery on 03/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
